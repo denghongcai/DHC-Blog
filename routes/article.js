688cf6bf1a55bcb390ea4766f756fd87992639d9
denghongcai <admin@dhchouse.com>
2013-08-03 21:08:35 +0800

    interfaces

---
+++ routes/article.js
@@ -0,0 +1,23 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: dhc
+ * Date: 13-8-3
+ * Time: 下午9:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+exports.read = function(req,res){
+    res.send("article content");
+};
+
+exports.add = function(req, res){
+    res.send("add successful!");
+};
+
+exports.del = function(req, res){
+    res.send("del successful!");
+};
+
+exports.modify = function(req, res){
+    res.send("modify successful!");
+};
